d842eb13c94da1ca015c7b4d1da8ef4a8c44274d
Mila-Kostic <milakostic.eng@gmail.com>
2022-05-02 17:49:55 +0200

    create options

--- main.js
+++ main.js
@@ -9,6 +9,10 @@ let booksViewBtn = document.querySelector('#books-view-btn');
 let newBookViewBtn = document.querySelector('#new-book-view-btn');
 let editDeleteViewBtn = document.querySelector('#edit-delete-btn');
 
+
+//FORMS
+let genreSelect = document.querySelector('#genre-select');
+let publisherSelect = document.querySelector('#publisher-select')
 // LISTENERS
 booksViewBtn.addEventListener('click', displayBooksView);
 newBookViewBtn.addEventListener('click', displayNewBookView);
@@ -28,6 +32,8 @@ function displayNewBookView(e) {
     if (e) {
         e.preventDefault()
     }
+    createGenericOptions();
+    createPublisherOptions();
     editDeleteView.style.display = "none";
     booksView.style.display = "none";
     newBookView.style.display = "block";
@@ -40,4 +46,26 @@ function displayEditDeleteBookView(e) {
     booksView.style.display = "none";
     newBookView.style.display = "none";
     editDeleteView.style.display = "block";
+}
+
+
+//Options
+function createGenericOptions() {
+    let text = '';
+    allGenre.forEach(genre => {
+        text += `
+        <option value "${genre}">${genre}</option>`
+        .trim()
+    })
+    genreSelect.innerHTML = text;
+}
+
+function createPublisherOptions(){
+    let text = '';
+    allPublisher.forEach(pub => {
+        text +=`
+        <option value="${pub}">${pub}</option>
+        `.trim()
+    })
+    publisherSelect.innerHTML = text;
 }
